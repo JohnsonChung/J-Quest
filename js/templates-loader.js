9242a060c054500355a747ec9b2fc33c72d19e6e
Johnson <johnson780510@gmail.com>
2015-03-29 02:45:54 +0800

    更換頁面時 會將讀取的 目標template名稱 console.log

--- js/templates-loader.js
+++ js/templates-loader.js
@@ -1,6 +1,25 @@
 $(function() {
     var templates = [
-        "templateBody","sideNav","about", "campaign", "concept", "faq", "howto", "privacy", "recruit", "recruitForm", "service", "shop"
+        "templateBody",
+        "sideNav",
+        "about",
+        "campaign",
+        "concept",
+        "faq",
+        "howto",
+        "privacy",
+        "recruit", 
+        "recruit/recruit-form",
+        "recruit/recruit-manager",
+        "recruit/recruit-merit",
+        "recruit/recruit-oshiete",
+        "recruit/recruit-parttimer",
+        "recruit/recruit-pts",
+        "recruit/recruit-voice",        
+        "recruit/recruit-work",
+        "recruitForm",
+        "service",
+        "shop"
     ];
     var templateCaches = {};
 
@@ -8,37 +27,38 @@ $(function() {
     var $main = $('#main');
     var $side = $('#side');
 
-    $.get('js/templates/concept.hbs').then(function(data) {    	
+    $.get('js/templates/recruit/recruit-manager.hbs').then(function(data) {
         $main.html(Handlebars.compile(data));
     });
-    $.get('js/templates/sideNav.hbs').then(function(data) {    	
+    $.get('js/templates/sideNav.hbs').then(function(data) {
         $side.html(Handlebars.compile(data));
     });
 
     var hbs = {
-    	cache: {},
-    	initialize: function() {
-    		this.cacheTemplate(templates);
-    		this.cache = templateCaches;
-    	},
-    	/* 將所有 template.hbs 塞到 templateCaches 裡面 */
-    	cacheTemplate: function(templates) {
-	    	$.each(templates, function( index, value) {
-	    		$.get('js/templates/'+ value + '.hbs').then(function(data) {    			
-	    			var o = {};
-	    			o[value] = data;
-	    			$.extend(templateCaches, o);
-	    		});
-	    	});
-    	},
-    	/* 點擊按鈕後切換 template */
-    	change: function(target) {
-    		$main.html( Handlebars.compile(this.cache[target]) );
-    	},
-    	laugth: function(e) {
-    		console.log(e);
-    	}
+        cache: {},
+        initialize: function() {
+            this.cacheTemplate(templates);
+            this.cache = templateCaches;
+        },
+        /* 將所有 template.hbs 塞到 templateCaches 裡面 */
+        cacheTemplate: function(templates) {
+            $.each(templates, function(index, value) {
+                $.get('js/templates/' + value + '.hbs').then(function(data) {
+                    var o = {};
+                    o[value] = data;
+                    $.extend(templateCaches, o);
+                });
+            });
+        },
+        /* 點擊按鈕後切換 template */
+        change: function(target) {
+            $main.html(Handlebars.compile(this.cache[target]));
+            console.log("change page\: handlebars template loaded [" + target + "]." );
+        },
+        laugth: function(e) {
+            console.log(e);
+        }
     };
     window.hbs = hbs;
     hbs.initialize();
-});	
+});
