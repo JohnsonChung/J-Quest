f95f7970fb08ead0d789fe0be36aace10a532419
Johnson <johnson780510@gmail.com>
2015-03-30 03:02:51 +0800

    加註

--- js/templates-loader.js
+++ js/templates-loader.js
@@ -1,4 +1,5 @@
 $(function() {
+    // 需要讀取的樣板列表
     var templates = [
         "templateBody",
         "sideNav",
@@ -22,31 +23,21 @@ $(function() {
         "service",
         "shop",
     ];
+    // 快取樣板
     var templateCaches = {};
 
-    /* cache elements */
+    // 快取 Elements
     var $main = $('#main');
     var $side = $('#side');
 
     var hbs = {
+        // 快取樣板
         cache: {},
-        registerPartial: function(e) {
-            Handlebars.registerPartial(e);
-            //
-            // undefined
-            //
-        },
-        compile: function(e) {
-            Handlebars.compile(e);
-            //
-            // undefined
-            //
-        },
         initialize: function() {
             this.cacheTemplate(templates);
             this.cache = templateCaches;
         },
-        /* 將所有 template.hbs 塞到 templateCaches 裡面 */
+        /* 將所有 template.hbs 快取到 templateCaches 裡面 */
         cacheTemplate: function(templates) {
             $.each(templates, function(index, value) {
                 $.get('js/templates/' + value + '.hbs').then(function(data) {
@@ -56,10 +47,12 @@ $(function() {
                 });
             });
         },
-        /* 點擊按鈕後切換 template */
+        /* 點擊按鈕後切換樣板 */
         change: function(target) {
             console.log("==== Start ====");
             var template ={};
+
+            // 如果有 "/" 則讀取次級樣板 (例如： recruit/recruit-form )
             if (target.lastIndexOf("/") != -1) {
                 console.log("讀取次級樣板...")
                 var parts = target.split('/');
@@ -71,13 +64,18 @@ $(function() {
                 console.log('已讀取次級樣板 [' + partial + "].");
 
             } else {
+                // 檢查有沒有次級樣板 (例如： templates/recruit/recruit-index.hsb )
                 if (this.cache[target+'/'+target+'-index']) {
                     console.log('發現目標有次級樣板...');                    
                     template = Handlebars.compile(this.cache[target+'/'+target+'-index']);
                     console.log('已讀取次級樣板 ['+target+ '-index]');
-                } else { template = Handlebars.compile(this.cache[target]);}
+                } 
+                // 讀取一般樣板 ( 例如： about.hsb )
+                else { 
+                    template = Handlebars.compile(this.cache[target]);
+                }
             }
-
+            // 輸出樣板
             $main.html(template());
             console.log("切換頁面: 已讀取樣板 [" + target + "].");
             console.log("==== End ====");
@@ -87,6 +85,7 @@ $(function() {
     window.hbs = hbs;
     hbs.initialize();
 
+    // 讀取左方瀏覽列表
     $.get('js/templates/sideNav.hbs').then(function(data) {
         $side.html(Handlebars.compile(data));
     });
