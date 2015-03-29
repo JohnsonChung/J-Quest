89107a9c3818c861f430e80ce41376dd6556dc91
Johnson <johnson780510@gmail.com>
2015-03-30 02:55:37 +0800

    增加 handbars compile 次級樣板。 切換樣板會console.log

--- js/templates-loader.js
+++ js/templates-loader.js
@@ -5,21 +5,22 @@ $(function() {
         "about",
         "campaign",
         "concept",
+        "contact",  
         "faq",
         "howto",
         "privacy",
-        "recruit", 
+        "recruit",
+        "recruit/recruit-index",
         "recruit/recruit-form",
         "recruit/recruit-manager",
         "recruit/recruit-merit",
         "recruit/recruit-oshiete",
         "recruit/recruit-parttimer",
         "recruit/recruit-pts",
-        "recruit/recruit-voice",        
+        "recruit/recruit-voice",
         "recruit/recruit-work",
-        "recruitForm",
         "service",
-        "shop"
+        "shop",
     ];
     var templateCaches = {};
 
@@ -27,15 +28,20 @@ $(function() {
     var $main = $('#main');
     var $side = $('#side');
 
-    $.get('js/templates/recruit/recruit-parttimer.hbs').then(function(data) {
-        $main.html(Handlebars.compile(data));
-    });
-    $.get('js/templates/sideNav.hbs').then(function(data) {
-        $side.html(Handlebars.compile(data));
-    });
-
     var hbs = {
         cache: {},
+        registerPartial: function(e) {
+            Handlebars.registerPartial(e);
+            //
+            // undefined
+            //
+        },
+        compile: function(e) {
+            Handlebars.compile(e);
+            //
+            // undefined
+            //
+        },
         initialize: function() {
             this.cacheTemplate(templates);
             this.cache = templateCaches;
@@ -52,13 +58,36 @@ $(function() {
         },
         /* 點擊按鈕後切換 template */
         change: function(target) {
-            $main.html(Handlebars.compile(this.cache[target]));
-            console.log("change page\: handlebars template loaded [" + target + "]." );
-        },
-        laugth: function(e) {
-            console.log(e);
+            console.log("==== Start ====");
+            var template ={};
+            if (target.lastIndexOf("/") != -1) {
+                console.log("讀取次級樣板...")
+                var parts = target.split('/');
+                var body = parts[0];
+                var partial = parts[parts.length - 1];
+                template = Handlebars.compile(this.cache[target]);
+                Handlebars.registerPartial("partial", this.cache[body + '/' + partial]);
+
+                console.log('已讀取次級樣板 [' + partial + "].");
+
+            } else {
+                if (this.cache[target+'/'+target+'-index']) {
+                    console.log('發現目標有次級樣板...');                    
+                    template = Handlebars.compile(this.cache[target+'/'+target+'-index']);
+                    console.log('已讀取次級樣板 ['+target+ '-index]');
+                } else { template = Handlebars.compile(this.cache[target]);}
+            }
+
+            $main.html(template());
+            console.log("切換頁面: 已讀取樣板 [" + target + "].");
+            console.log("==== End ====");
         }
     };
+
     window.hbs = hbs;
     hbs.initialize();
+
+    $.get('js/templates/sideNav.hbs').then(function(data) {
+        $side.html(Handlebars.compile(data));
+    });
 });
